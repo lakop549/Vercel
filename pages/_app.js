dbc24ca59335b1ac390f261d92e04532287fc608
Joo Yeon <lakop549@naver.com>
2023-06-11 20:47:26 +0900

    A

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,36 @@
-import "../styles/globals.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Script from "next/script";
-import { SessionProvider } from "next-auth/react";
+import '../styles/globals.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Script from 'next/script'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <SessionProvider session={session}>
         <Script
-          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=c56166a3a9af6a1f0f372552f619fce4&libraries=services,clusterer&autoload=false"
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9441768b85cb4f99451d95174d2b8f07&libraries=services,clusterer&autoload=false"
           strategy="beforeInteractive"
         />
         <Component {...pageProps} />
       </SessionProvider>
     </>
-  );
+  )
 }
 
 function Auth({ children }) {
-  const router = useRouter();
+  const router = useRouter()
   const { status } = useSession({
     required: true,
     onUnauthenticated() {
-      router.push("/unauthorized?message=login required");
+      router.push('/unauthorized?message=login required')
     },
-  });
-  if (status === "loading") {
-    return <div>Loading...</div>;
+  })
+  if (status === 'loading') {
+    return <div>Loading...</div>
   }
 
-  return children;
+  return children
 }
 
-export default MyApp;
+export default MyApp
